4583575a2c2c0b8d1eb7bc19155d14be325e581e
Nama Anda <fahriabdurahmansoleh@gmail.com>
2025-08-01 22:16:30 +0700

    fix any

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-import type { NextConfig } from "next";
-
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
