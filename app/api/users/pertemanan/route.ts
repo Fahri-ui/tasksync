5a5fd9b41026e74358b970d76ab85251a226701b
Nama Anda <fahriabdurahmansoleh@gmail.com>
2025-08-01 23:34:26 +0700

    fix next-auth v2

--- app/api/users/pertemanan/route.ts
+++ app/api/users/pertemanan/route.ts
@@ -1,7 +1,7 @@
 // app/api/users/pertemanan/route.ts
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"; // Sesuaikan path
+import { authOptions } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 
 // GET: Ambil daftar user + status pertemanan
