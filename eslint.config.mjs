954bf0de7e80711351c015d05855a96b178468e9
Nama Anda <fahriabdurahmansoleh@gmail.com>
2025-08-01 22:37:42 +0700

    fix any v3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,7 @@
-// ✅ eslint.config.mjs - Versi yang benar
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 export default [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 ];
