4e9fe5caf6f345de0edf65191e606e04ad84f785
Nama Anda <fahriabdurahmansoleh@gmail.com>
2025-08-02 18:31:41 +0700

    fix next-auth v3

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
 // lib/auth.ts
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export { authOptions };
 export const getSession = () => getServerSession(authOptions);
