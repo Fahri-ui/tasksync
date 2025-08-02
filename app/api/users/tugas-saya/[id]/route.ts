4b20b5266e358356898b0f27c43f0d4ac3b6bd72
Nama Anda <fahriabdurahmansoleh@gmail.com>
2025-08-02 15:16:05 +0700

    fix params v5

--- app/api/users/tugas-saya/[id]/route.ts
+++ app/api/users/tugas-saya/[id]/route.ts
@@ -3,10 +3,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 
-export async function PATCH(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function PATCH(request: NextRequest, { params }: any) {
   try {
     const session = await getServerSession(authOptions);
     if (!session || !session.user) {
@@ -50,10 +47,7 @@ export async function PATCH(
   }
 }
 
-export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function DELETE(request: NextRequest, { params }: any) {
   try {
     const session = await getServerSession(authOptions);
     if (!session || !session.user) {
